7a9b3c16c762beb12f08b2fa3bcb8002fccaba93
Laksh Shetty <lakshshetty16@gmail.com>
2023-07-01 12:45:08 +0530

    delete later if needed

---
+++ Practice c++/untitled folder/ptr.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 100;
+    int*p = &a;
+    cout<<a<<endl;
+    cout<<*p<<endl;
+    cout<<p<<endl;
+    cout<<&a<<endl;
+}
---
+++ Practice c++/untitled folder/vmrkvgl.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N,X,K,P;
+	cin>>N>>X>>K>>P;
+	int r1=P+(K*10);
+	cout<<r1;
+	return 0;
+}
